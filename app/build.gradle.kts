4d0e00c5e03a6cbbedb3daad6bcc13b00b85e54a
Thais Sardinha <thais.sardinhasouzasoares@student.griffith.ie>
2023-11-28 13:00:57 +0000

    Design UI for Welcome Screen.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
 
+    implementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
     //Added
     implementation("androidx.compose.foundation:foundation:1.5.4")
     implementation("androidx.compose.ui:ui:1.5.4")
@@ -67,6 +70,8 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.8.0")
     implementation("androidx.navigation:navigation-runtime-ktx:2.7.5")
     implementation("androidx.navigation:navigation-compose:2.7.5")
+
+//    implementation("androidx.compose.material:material-icons-extended:$compose_ui_version")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/com/example/expensetracker/WelcomeScreen.kt
+++ app/src/main/java/com/example/expensetracker/WelcomeScreen.kt
@@ -6,66 +6,119 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun WelcomeScreen(onAddExpensesClick: () -> Unit, onViewExpensesClick: () -> Unit) {
-    MaterialTheme {
+    MaterialTheme{
         Box(
-            // applying some design
             modifier = Modifier
                 .fillMaxSize()
-                .background(Color.LightGray)
+                .background(Color.Black)
                 .padding(16.dp)
         ) {
+            // Centralized and styled welcome text
+            Text(
+                text = "Welcome",
+                color = Color(0xFF03DAC6),
+                style = MaterialTheme.typography.h3, // Use h3 for a larger font
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .align(Alignment.TopCenter) // Align to the top center of the screen
+            )
             Column(
-                // applying some design
                 modifier = Modifier.fillMaxSize(),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Image(
-                    // adding image to welcome screen --- don't think I will keep it
-                    painter = painterResource(id = R.drawable.dw),
-                    contentDescription = "Digital Wallet"
+                    painter = painterResource(id = R.drawable.cover),
+                    contentDescription = "Digital Wallet",
+                    modifier = Modifier
+                        .fillMaxWidth(0.8f) // Ensure the Image takes the full width
+                        .height(300.dp) // Adjust the height as needed
+                        .clip(RoundedCornerShape(16.dp)) // Apply rounded corners
                 )
 
-                // i am not entirely happy with my design at the moment
-                // so i will probably try and enhance it in the future milestones
                 Text("Welcome to Expense Tracker", style = MaterialTheme.typography.h5)
                 Spacer(modifier = Modifier.height(32.dp))
-                Button(
-                    // setting what to do if user clicks on this button
+                ButtonWithIcon(
                     onClick = { onAddExpensesClick() },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(50.dp),
-                    shape = RoundedCornerShape(8.dp),
-                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.Gray)
-                ) {
-                    Text("Add Expenses", color = Color.White)
-                }
-                Spacer(modifier = Modifier.height(16.dp))
-                Button(
-                    // setting what to do if user clicks on this button
+                    icon = "plus",
+                    buttonText = "Add Expenses"
+                )
+                Spacer(modifier = Modifier.height(35.dp))
+                ButtonWithIcon(
                     onClick = { onViewExpensesClick() },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(50.dp),
-                    shape = RoundedCornerShape(8.dp),
-                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.DarkGray)
-                ) {
-                    Text("View Expenses", color = Color.White)
+                    icon = "plus", // Replace with the actual eye icon resource ID
+                    buttonText = "View Expenses"
+                )
+                Spacer(modifier = Modifier.height(3.dp))
+            }
+        }
+    }
+}
+
+@Composable
+fun ButtonWithIcon(
+    onClick: () -> Unit,
+    backgroundColor: Color = Color(0xFF03DAC6),
+    icon: Any? = null,
+    buttonText: String
+) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .fillMaxWidth(0.8f)
+            .height(70.dp),
+        shape = RoundedCornerShape(10.dp),
+        colors = ButtonDefaults.buttonColors(backgroundColor = backgroundColor)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            icon?.let {
+                if (it is Int) {
+                    Image(
+                        painter = painterResource(id = it),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(32.dp)
+                            .padding(end = 12.dp)
+                    )
+                } else if (it is ImageVector) {
+                    Icon(
+                        imageVector = it,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(32.dp)
+                            .padding(end = 12.dp)
+                    )
+                } else if (it is String && it == "plus") {
+                    Text("+", color = Color.Black, fontSize = 32.sp, fontWeight = FontWeight.Bold)
+                    Spacer(modifier = Modifier.width(8.dp))
                 }
             }
+            Text(buttonText, color = Color.Black, fontSize = 22.sp)
         }
     }
 }
